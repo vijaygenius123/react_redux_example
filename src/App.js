9c98b9c1133bfe98c19d7c4681b03f5494d8b1bf
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-07-13 22:16:56 +0530

    Static UI

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h2>Welcome To React With Redux</h2>
+      </div>
+      <div className="Todo-App">
+        <form>
+          <input type="text" />
+        </form>
+        <div className="Todo-List">
+          <ul>
+            <li>
+              <input type="checkbox" /> Create Static UI
+            </li>
+            <li>
+              <input type="checkbox" /> Create Initial State
+            </li>
+            <li>
+              <input type="checkbox" /> Use State To Render UI
+            </li>
+          </ul>
+        </div>
+      </div>
+
+
     </div>
   );
 }
