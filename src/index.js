42d962cae3e90dfaa7e922e7750430b38cdce412
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-07-14 10:52:56 +0530

    Basic Subscribe & Dispatch

--- src/index.js
+++ src/index.js
@@ -5,14 +5,22 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store'
 
+const render = () => {
+  const state = store.getState()
+  ReactDOM.render(
+    <React.StrictMode>
+      <App {...state} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
-const state = store.getState()
-ReactDOM.render(
-  <React.StrictMode>
-    <App {...state} />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+render()
+store.subscribe(render)
+
+setTimeout(() => {
+  store.dispatch({ type: "TODO_ADD", payload: { id: 4, name: 'Add Todo From Dispatch', isCompleted: false } })
+}, 1000)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
